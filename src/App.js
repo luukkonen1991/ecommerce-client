36ca2f2be9b70766b46219303591375899143119
luukkonen1991 <luukkonen1991@gmail.com>
2020-06-24 15:37:36 +0300

    ADDED layout HOC and navigatin/toolbar component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import React from 'react';
-import './App.css';
 
-function App() {
+import './App.css';
+import Layout from './hoc/Layout/Layout';
+import Home from './containers/Home/Home';
+
+const App = props => {
   return (
-    <div className="App">
-      <h1>HELLO</h1>
-    </div>
+    <Layout>
+      <Home />
+    </Layout>
   );
-}
+};
 
 export default App;
---
+++ src/components/Navigation/Toolbar/Toolbar.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Toolbar() {
+  return (
+    <div>
+      <h1>I am a toolbar</h1>
+    </div>
+  );
+}
+
+export default Toolbar;
