3356c9a8ff8d6617568ea0b2f02ad3dcc2ff194c
luukkonen1991 <luukkonen1991@gmail.com>
2020-06-24 16:38:24 +0300

    ADDED react-router and react-router-dom dependicies and ADDED swtich/router to project

--- src/App.js
+++ src/App.js
@@ -1,13 +1,21 @@
 import React from 'react';
+import { Route, Switch, Redirect } from 'react-router-dom';
 
 import './App.css';
 import Layout from './hoc/Layout/Layout';
 import Home from './containers/Home/Home';
 
+let routes = (
+  <Switch>
+    <Route path="/" exact component={Home} />
+    <Redirect to="/" />
+  </Switch>
+);
+
 const App = props => {
   return (
     <Layout>
-      <Home />
+      {routes}
     </Layout>
   );
 };
