78e0a415c7e171ff78c076aa352480ade82740fe
luukkonen1991 <luukkonen1991@gmail.com>
2020-06-25 15:09:27 +0300

    NEW BRANCH

--- src/App.js
+++ src/App.js
@@ -1,15 +1,23 @@
 
 import React from 'react';
+import { Route, Switch, Redirect } from 'react-router-dom';
 
 import './App.css';
 import Layout from './hoc/Layout/Layout';
 import Home from './containers/Home/Home';
 
 
+let routes = (
+  <Switch>
+    <Route path="/" component={Home} />
+    <Redirect to="/" />
+  </Switch>
+);
+
 const App = props => {
   return (
     <Layout>
-      <Home />
+      {routes}
     </Layout>
 
   );
