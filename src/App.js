bc70220e2cc63f6d805f6022da28992bfd23035c
luukkonen1991 <luukkonen1991@gmail.com>
2020-06-25 14:21:23 +0300

    MERGE ERROR

--- src/App.js
+++ src/App.js
@@ -1,21 +1,13 @@
 import React from 'react';
-import { Route, Switch, Redirect } from 'react-router-dom';
 
 import './App.css';
 import Layout from './hoc/Layout/Layout';
 import Home from './containers/Home/Home';
 
-let routes = (
-  <Switch>
-    <Route path="/" exact component={Home} />
-    <Redirect to="/" />
-  </Switch>
-);
-
 const App = props => {
   return (
     <Layout>
-      {routes}
+      <Home />
     </Layout>
   );
 };
