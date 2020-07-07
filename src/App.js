b7cf88c9bd43e40033159ae78f8b0360ebefec50
Lauri Kemppainen <laukempp@gmail.com>
2020-07-07 13:57:06 +0300

    Created register form, css modifications

--- src/App.js
+++ src/App.js
@@ -5,12 +5,14 @@ import "./App.css";
 import Layout from "./hoc/Layout/Layout";
 import Home from "./containers/Home/Home";
 import SignInAndSignUp from "./containers/SignInAndSignUp/SignInAndSignUp";
-import ContactForm from './containers/ContactForm/ContactForm';
+import ContactForm from "./containers/ContactForm/ContactForm";
+import SignInForm from "./components/Sign-in-form/Sign-in-form";
 
 let routes = (
   <Switch>
     <Route exact path="/" component={Home} />
     <Route path="/signin" component={SignInAndSignUp} />
+    <Route path="/register" component={SignInAndSignUp} />
     <Route path="/contact" component={ContactForm} />
     <Redirect to="/" />
   </Switch>
