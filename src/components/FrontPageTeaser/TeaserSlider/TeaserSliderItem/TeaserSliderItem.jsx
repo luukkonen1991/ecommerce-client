f605b9a6f4475963f6e93318cae597f41e447637
luukkonen1991 <luukkonen1991@gmail.com>
2020-07-17 19:57:24 +0300

    CHANGED how teaserSlider works and removed repetition

---
+++ src/components/FrontPageTeaser/TeaserSlider/TeaserSliderItem/TeaserSliderItem.jsx
@@ -0,0 +1,33 @@
+import React, { useEffect } from "react";
+import "./TeaserSliderItem.scss";
+import men from "./men.png";
+import women from "./womens.png";
+import boys from "./men.png";
+import girls from "./womens.png";
+
+
+const TeaserSliderItem = (props) => {
+  const titleArr = ['MEN', 'WOMEN', 'BOYS', 'GIRLS'];
+  const imgArr = [men, women, boys, girls];
+
+  const { count } = props;
+
+  useEffect(() => {
+    console.log('props', count);
+  }, [count]);
+
+  return (
+    <div className="item-promo">
+      <div
+        className="background-image"
+        style={{ backgroundImage: `url(${imgArr[count]})` }}
+      ></div>
+      <div className="content">
+        <h1 className="title">{titleArr[count]}</h1>
+        <span className="subtitle">SHOP NOW</span>
+      </div>
+    </div>
+  );
+};
+
+export default TeaserSliderItem;
