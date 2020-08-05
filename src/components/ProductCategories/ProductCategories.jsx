b8f3fb67a6bd50270f2aec51c58e1cf4b5063aa0
luukkonen1991 <luukkonen1991@gmail.com>
2020-08-05 22:28:39 +0300

    TRYING to get products and categories displayed

--- src/components/ProductCategories/ProductCategories.jsx
+++ src/components/ProductCategories/ProductCategories.jsx
@@ -5,22 +5,49 @@ import { fetchAllProducts } from '../../services/ProductService';
 import "./ProductCategories.scss";
 
 const ProductCategories = props => {
-  const [targetId, setTargetId] = useState();
+  // const [targetId, setTargetId] = useState();
   const [targetGroupProducts, setTargetGroupProducts] = useState();
+  const [targetCategories, setTargetCategories] = useState([]);
+  let categoryContainerContent;
+  let categoryProductContent;
 
   useEffect(() => {
+    console.log('PRODUCTCATEGORIES RENDERED');
     console.log('[ProductCategoriesComponent Rendered]');
     console.log(props.match);
     console.log(props.location.state.targetId);
     fetchAllProducts({ params: { targetGroupId: props.location.state.targetId, include: "category" } })
       .then(resp => {
+        setTargetGroupProducts(resp.data.data);
+        // setTargetCategories()
         console.log(resp.data.data);
+        for (let data of resp.data.data) {
+          if (targetCategories.indexOf(data.category.title) === -1) {
+            setTargetCategories([...targetCategories, data.category.title]);
+          }
+        }
+        // console.log(Object.values(resp.data.data[0]), '[VALUES]');
       });
-  }, [props]);
+  }, [props, targetCategories]);
+
+
+  console.log(targetCategories, '[TARGETCATEGORIES]');
+
+  categoryContainerContent =
+
+    categoryProductContent = targetCategories.map((category) => {
+      let products = targetGroupProducts.filter(product => { return product.category.title === category; });
+      console.log(products, '[PRODUCTS .,.fadsf]');
+      return (
+        <div className="category-container" >
+
+        </div>
+      );
+    });
 
   return (
     <div className="main-container">
-      <p>hello</p>
+      <p>i am the main-container</p>
       <div className="category-container" >
         <div className="category-img" >
           <p>hello</p>
