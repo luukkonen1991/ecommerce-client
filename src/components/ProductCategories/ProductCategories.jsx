b3e684bffc9c0504e8023de42783fa65a8b8ad12
luukkonen1991 <luukkonen1991@gmail.com>
2020-08-05 21:32:55 +0300

    SUCCESFULLY fetch products with targetGroupId and include category table

--- src/components/ProductCategories/ProductCategories.jsx
+++ src/components/ProductCategories/ProductCategories.jsx
@@ -1,20 +1,34 @@
 import React, { useEffect, useState } from 'react';
 
-import { } from '../../services/TargetGroupService';
+// import { } from '../../services/TargetGroupService';
+import { fetchAllProducts } from '../../services/ProductService';
 import "./ProductCategories.scss";
 
 const ProductCategories = props => {
   const [targetId, setTargetId] = useState();
+  const [targetGroupProducts, setTargetGroupProducts] = useState();
 
   useEffect(() => {
     console.log('[ProductCategoriesComponent Rendered]');
     console.log(props.match);
     console.log(props.location.state.targetId);
+    fetchAllProducts({ params: { targetGroupId: props.location.state.targetId, include: "category" } })
+      .then(resp => {
+        console.log(resp.data.data);
+      });
   }, [props]);
 
   return (
     <div className="main-container">
-      <h1>Hello im ProductCategoriesComp</h1>
+      <p>hello</p>
+      <div className="category-container" >
+        <div className="category-img" >
+          <p>hello</p>
+        </div>
+        <div className="category-content" >
+          <p>hello</p>
+        </div>
+      </div>
     </div>
   );
 };
