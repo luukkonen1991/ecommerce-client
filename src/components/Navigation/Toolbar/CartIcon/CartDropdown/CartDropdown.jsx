00f98a47983bdef75760fee0c8f5ad5ceed9021b
Lauri Kemppainen <laukempp@gmail.com>
2020-08-12 14:41:29 +0300

    created CartPage component, added cartItemCount to cartIcon

--- src/components/Navigation/Toolbar/CartIcon/CartDropdown/CartDropdown.jsx
+++ src/components/Navigation/Toolbar/CartIcon/CartDropdown/CartDropdown.jsx
@@ -1,21 +1,28 @@
 import React from "react";
 import { connect } from "react-redux";
+import { withRouter } from "react-router-dom";
 
 import FormButton from "../../../../UI/Form/FormButton/FormButton";
 import CartItem from "../../../../CartItem/CartItem";
 
 import "./CartDropdown.scss";
 
-const CartDropdown = ({ cartItems }) => {
+const CartDropdown = ({ cartItems, history }) => {
   console.log(cartItems);
   return (
     <div className="cart-dropdown">
       <div className="cart-items">
-        {cartItems.map((cartItem) => (
-          <CartItem key={cartItem.id} item={cartItem} />
-        ))}
+        {cartItems.length ? (
+          cartItems.map((cartItem) => (
+            <CartItem key={cartItem.id} item={cartItem} />
+          ))
+        ) : (
+          <span className="empty-message">Your cart is empty</span>
+        )}
       </div>
-      <FormButton>GO TO CHECKOUT</FormButton>
+      <FormButton onClick={() => history.push("/cart")}>
+        GO TO CHECKOUT
+      </FormButton>
     </div>
   );
 };
@@ -24,4 +31,4 @@ const mapStateToProps = (state) => ({
   cartItems: state.cart.cartItems,
 });
 
-export default connect(mapStateToProps)(CartDropdown);
+export default withRouter(connect(mapStateToProps)(CartDropdown));
---
+++ src/containers/CartPage/CartPage.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+import "./CartPage.scss";
+
+const CartPage = () => (
+  <div className="checkout-page">
+    <div className="checkout-header">
+      <div className="header-block">
+        <span></span>
+      </div>
+    </div>
+  </div>
+);
+
+export default CartPage;
