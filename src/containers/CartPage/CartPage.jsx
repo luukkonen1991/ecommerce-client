3eb43024873aae3c1b373322eeb7d4aeaa86b898
Lauri Kemppainen <laukempp@gmail.com>
2020-08-18 14:10:23 +0300

    added CartPage component and CartPageItem components

--- src/containers/CartPage/CartPage.jsx
+++ src/containers/CartPage/CartPage.jsx
@@ -1,15 +1,37 @@
 import React from "react";
+import { connect } from "react-redux";
+
+import CartPageItem from "../../components/CartPageItem/CartPageItem";
 
 import "./CartPage.scss";
 
-const CartPage = () => (
-  <div className="checkout-page">
-    <div className="checkout-header">
-      <div className="header-block">
-        <span></span>
+const CartPage = ({ cartItems, totalPrice }) => {
+  return (
+    <div className="cart-page">
+      <div className="cart-page-header">
+        <div className="title">
+          <h2 className="cart-title">YOUR CART</h2>
+        </div>
+      </div>
+
+      <div className="cart-page-items">
+        {cartItems.map((cartItem) => (
+          <CartPageItem key={cartItem.id} cartItem={cartItem} />
+        ))}
+      </div>
+      <div className="total">
+        <span>TOTAL: ${totalPrice}</span>
       </div>
     </div>
-  </div>
-);
+  );
+};
 
-export default CartPage;
+const mapStateToProps = ({ cart: { cartItems } }) => ({
+  cartItems: cartItems,
+  totalPrice: cartItems.reduce(
+    (accumulator, cartItem) => accumulator + cartItem.quantity * cartItem.price,
+    0
+  ),
+});
+
+export default connect(mapStateToProps)(CartPage);
